dda56a99ee9c26c28e5e40c33dbeb6b56bdea37a
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-12-13 10:37:38 +0300

    Created drawer navigation activity

---
+++ app/src/main/java/com/recip/ui/activities/MainActivity.java
@@ -0,0 +1,150 @@
+package com.recip.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.recip.R;
+import com.recip.ui.fragments.LoginActivity;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+
+
+    private AppBarConfiguration mAppBarConfiguration;
+    public DrawerLayout drawer;
+    NavController navController;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        setUpNavigation();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Menu menu = navigationView.getMenu();
+        MenuItem menuItem = menu.findItem(R.id.nav_logout);
+
+        if (mAuth.getCurrentUser() == null) {
+            menuItem.setTitle("Login");
+
+        } else {
+            menuItem.setTitle("Logout");
+        }
+
+    }
+
+    private void setUpNavigation() {
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_latest, R.id.nav_categories,
+                R.id.nav_views, R.id.nav_favourites, R.id.nav_settings, R.id.nav_logout)
+                .setDrawerLayout(drawer)
+                .build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_icon:
+                onSearchRequested();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        menuItem.setChecked(true);
+
+        drawer.closeDrawers();
+
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.nav_home:
+                navController.navigate(R.id.nav_home);
+                break;
+            case R.id.nav_latest:
+                navController.navigate(R.id.nav_latest);
+                break;
+            case R.id.nav_categories:
+                navController.navigate(R.id.nav_categories);
+                break;
+            case R.id.nav_views:
+                navController.navigate(R.id.nav_views);
+                break;
+            case R.id.nav_favourites:
+                navController.navigate(R.id.nav_favourites);
+                break;
+            case R.id.nav_settings:
+                navController.navigate(R.id.nav_settings);
+                break;
+            case R.id.nav_logout:
+                if (menuItem.getTitle().equals("Logout")) {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                } else {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+                }
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onSearchRequested() {
+        return super.onSearchRequested();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
