43304ff8ffe88c10737020de50b54b3035589e8d
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-12-16 14:25:32 +0300

    Create API constants

---
+++ app/src/main/java/com/recip/Constants.java
@@ -0,0 +1,6 @@
+package com.recip;
+
+public class Constants {
+    public static final String RECIPE_BASE_URL = "https://api.spoonacular.com/recipes/";
+    public static final String RECIPE_API_KEY = BuildConfig.RECIPE_API_KEY;
+}
