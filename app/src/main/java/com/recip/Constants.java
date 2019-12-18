6c3adad2272ca695a1bd1d037e3390d15516f47e
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-12-18 14:50:05 +0300

    Refactor Sign Up class

--- app/src/main/java/com/recip/Constants.java
+++ app/src/main/java/com/recip/Constants.java
@@ -1,6 +1,7 @@
 package com.recip;
 
 public class Constants {
+    public static final int GALLERY_REQUEST_CODE = 100;
     public static final String RECIPE_BASE_URL = "https://api.spoonacular.com/recipes/";
     public static final String RECIPE_API_KEY = BuildConfig.RECIPE_API_KEY;
 }
