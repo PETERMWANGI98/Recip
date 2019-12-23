d9a34eadf2cd2ac8b90f649aac9212123295c4b6
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-12-23 16:39:49 +0300

    Handle click of menu item

--- app/src/main/java/com/recip/ui/viewholders/MenuViewHolder.java
+++ app/src/main/java/com/recip/ui/viewholders/MenuViewHolder.java
@@ -1,5 +1,8 @@
 package com.recip.ui.viewholders;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,12 +11,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.recip.R;
+import com.recip.ui.activities.MoreRecipesActivity;
 
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MenuViewHolder extends RecyclerView.ViewHolder {
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     @BindView(R.id.mImageView)
     public ImageView mImageView;
 
@@ -21,8 +25,21 @@ public class MenuViewHolder extends RecyclerView.ViewHolder {
     public TextView mTitle;
 
 
-    public MenuViewHolder(@NonNull  View itemView) {
+    public MenuViewHolder(@NonNull View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
+        itemView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //catch the title and load MoreRecipesActivity
+        Context mContext=v.getContext();
+        Intent intent=new Intent(mContext, MoreRecipesActivity.class);
+        intent.putExtra("title",mTitle.getText().toString().trim());
+        mContext.startActivity(intent);
+
     }
 }
