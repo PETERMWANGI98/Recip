d48fac5a2b1c1afd8569a3dc9aa408e133b4144b
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-12-23 16:43:23 +0300

    Create a search endpoint

--- app/src/main/java/com/recip/network/RecipeApi.java
+++ app/src/main/java/com/recip/network/RecipeApi.java
@@ -4,10 +4,17 @@ import com.recip.models.RecipeRandomResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface RecipeApi {
 
     @GET("random?number=4&tags=vegetarian,dessert")
     Call<RecipeRandomResponse> getRecommendedRecipes();
+
+    @GET("search")
+    Call<RecipeRandomResponse> getRecipeFromTitle(
+            @Query("query") String query,
+            @Query("number") String number
+    );
 
 }
